401e63e777b97f03cd08279250e178908c89fd38
aazpeitia <aazpeitia@vicomtech.org>
2014-02-04 10:43:18 +0100

    encoding error replaces

--- core/text-fixer.pl
+++ core/text-fixer.pl
@@ -22,13 +22,10 @@ sub fix_text {
 	
 	chomp($text);
 	
-	#substitutes
-	$text =~ s/â€¦/…/g; # elipsis
-	$text =~ s/â€“/–/g; # long hyphen
-	$text =~ s/â€™/’/g; #curly apostrophe
-	$text =~ s/â€œ/“/g; # curly open quote
-	$text =~ s/â€/”/g; # curly close quote
+	#fix encoding
+	$text = &fix_encoding($text);
 	
+
 	#word token method
 	my @words = split(/\s/,$text);
 	$text = "";
@@ -78,6 +75,92 @@ sub fix_text {
 	#freedoms." 'Outrageous'Although => freedoms". 'Outrageous' Although
 	#$text =~ s/(\")([^\"]+)(\. ?)(\")/$1$2$4$3/g;
 	#$text =~ s/(\')([^\']+)(\. ?)(\')/$1$2$4$3/g;
+	return $text;
+}
+
+sub fix_encoding {
+
+	my $text = shift(@_);	
+
+	$text =~ s/â/'/g;
+	$text =~ s/Ã/À/g;
+	$text =~ s//“/g;
+############################################
+############################################
+	$text =~ s/â€¦/…/g; # elipsis
+	$text =~ s/â¦/…/g; # elipsis
+	$text =~ s/â€“/–/g; # long hyphen
+	$text =~ s/â€™/’/g; #curly apostrophe
+	$text =~ s/â€œ/“/g; # curly open quote
+	$text =~ s/â€/”/g; # curly close quote
+	$text =~ s/Â»/»/g;
+	$text =~ s/Â«/«/g;
+############################################
+	$text =~ s/Ã¡/á/g;
+	$text =~ s/Ã©/é/g;
+	$text =~ s/Ã\*/í/g;
+	$text =~ s/Ã³/ó/g;
+	$text =~ s/Ãº/ú/g;
+
+	$text =~ s/Ã/Á/g;
+	$text =~ s/Ã‰/É/g;
+	$text =~ s/Ã/Í/g;
+	$text =~ s/Ã“/Ó/g;
+	$text =~ s/Ãš/Ú/g;
+############################################
+	$text =~ s/Ã±/ñ/g;
+	$text =~ s/Ã§/ç/g;
+	$text =~ s/Å“/œ/g;
+
+	$text =~ s/Ã‘/Ñ/g;
+	$text =~ s/Ã‡/Ç/g;
+	$text =~ s/Å’/Œ/g;
+############################################
+	$text =~ s/Â©/©/g;
+	$text =~ s/Â®/®/g;
+	$text =~ s/â„¢/™/g;
+	$text =~ s/Ã˜/Ø/g;
+	$text =~ s/Âª/ª/g;
+############################################
+	$text =~ s/Ã¤/ä/g;
+	$text =~ s/Ã«/ë/g;
+	$text =~ s/Ã¯/ï/g;
+	$text =~ s/Ã¶/ö/g;
+	$text =~ s/Ã¼/ü/g;
+
+	$text =~ s/Ã„/Ä/g;
+	$text =~ s/Ã‹/Ë/g;
+	$text =~ s/Ã /Ï/g;
+	$text =~ s/Ã– /Ö/g;
+	$text =~ s/Ãœ/Ü/g;
+############################################
+	$text =~ s/Ã /à/g;
+	$text =~ s/Ã¨/è/g;
+	$text =~ s/Ã¬/ì/g;
+	$text =~ s/Ã²/ò/g;
+	$text =~ s/Ã¹/ù/g;
+
+	$text =~ s/Ã€/À/g;
+	$text =~ s/Ãˆ/È/g;
+	$text =~ s/ÃŒ/Ì/g;
+	$text =~ s/Ã’/Ò/g;
+	$text =~ s/Ã™/Ù/g;
+############################################
+	$text =~ s/Ã¢/â/g;
+	$text =~ s/Ãª/ê/g;
+	$text =~ s/Ã®/î/g;
+	$text =~ s/Ã´/ô/g;
+	$text =~ s/Ã»/û/g;
+
+	$text =~ s/Ã‚/Â/g;
+	$text =~ s/ÃŠ/Ê/g;
+	$text =~ s/ÃŽ/Î/g;
+	$text =~ s/Ã”/Ô/g;
+	$text =~ s/Ã›/Û/g;
+############################################
+	$text =~ s/Ã/E/g;
+
+
 	return $text;
 }
 
